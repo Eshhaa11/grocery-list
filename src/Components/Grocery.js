e35a9a79ed41fa70673cd3dea9d5da1badbd00ff
Esha Patel <Patellesha11@gmail.com>
2025-04-08 10:22:20 +0300

    Added

--- src/Components/Grocery.js
+++ src/Components/Grocery.js
@@ -1,61 +1,61 @@
 import { useState } from "react";
 
-function Grocery () {
+function Grocery() {
+  const [grocitem, setGrocItem] = useState([]);
+  const [newGroc, setNewGroc] = useState("");
 
-    const [grocitem , setGrocItem] = useState([]);
-    const [newGroc , setNewGroc] = useState('');
- 
-    function addItem() {
-        if (newGroc === "") return;
-        setNewGroc ([...grocitem, { name: newGroc, checked: false }]);
-        setNewItem("");
+  function addItem() {
+    if (newGroc === "") return;
+    setNewGroc([...grocitem, { name: newGroc, checked: false }]);
+    setNewItem("");
+  }
 
-    }
+  function toggleCheck(index) {
+    const updatedGroc = [...grocitem];
+    updatedGroc[index].checked = !updatedGroc[index].checked;
+    setGrocItem(updatedGroc);
+  }
 
-    function toggleCheck(index) {
-        const updatedGroc = [...grocitem];
-        updatedGroc[index].checked = !updatedGroc[index].checked;
-        setGrocItem(updatedGroc);
-    }
+  function deleteGroc(index) {
+    const updatedGroc = [...grocitem];
+    updatedGroc.splice(index, 1);
+    setGrocItem(updatedGroc);
+  }
 
-    function deleteGroc(index) {
-        const updatedGroc = [...grocitem];
-        updatedGroc.splice(index , 1);
-        setGrocItem(updatedGroc);
-    }
+  return (
+    <div className="container">
+      <h2>Grocery List</h2>
+      <input
+        value={newGroc}
+        onChange={(e) => setNewGroc(e.target.value)}
+        placeholder="Add Grocery"
+      />
 
+      <button onClick={addGrocItem}>Add</button>
 
-    return (
-        <div className="container">
-        <h2>Grocery List</h2>
-        <input
-          value={newGroc}
-          onChange={(e) => setNewGroc(e.target.value)}
-          placeholder="Add Grocery"
-        />
-       
-       <button onClick={addGrocItem}>Add</button>
-    
-       <ul>
+      <ul>
         {grocitem.map((grocitem, index) => (
-            <li key={index}>
-            <input 
-             type="checkbox"
-             checked= {grocitem.checked}
-             onChange={() => toggleCheck(index)} 
-             />
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={grocitem.checked}
+              onChange={() => toggleCheck(index)}
+            />
 
-            <span style={{textDecoration: grocitem.checked ? "line-through" : "" }}>
-                {grocitem.name}
-            </span> 
+            <span
+              style={{ textDecoration: grocitem.checked ? "line-through" : "" }}
+            >
+              {grocitem.name}
+            </span>
 
-           <button onClick={() => deletegrocitem(index)}>Delete Grocery</button>
-            </li>
+            <button onClick={() => deletegrocitem(index)}>
+              Delete Grocery
+            </button>
+          </li>
         ))}
-       </ul>
-            
-        </div>
-    )
+      </ul>
+    </div>
+  );
 }
 
 export default Grocery;
